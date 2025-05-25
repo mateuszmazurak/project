de66877d9ba1f0ccc9e4a6413b2cbcf3d8b47211
Mazur <mateuszmazurak@gmail.com>
2025-05-25 20:43:06 +0200

    Backup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-get "/products", to: "products#index"
-
-get "/products/new", to: "products#new"
-post "/products", to: "products#create"
-
-get "/products/:id", to: "products#show"
-
-get "/products/:id/edit", to: "products#edit"
-patch "/products/:id", to: "products#update"
-put "/products/:id", to: "products#update"
-
-delete "/products/:id", to: "products#destroy"
-
+  root "products#index"
+  resources :products
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
